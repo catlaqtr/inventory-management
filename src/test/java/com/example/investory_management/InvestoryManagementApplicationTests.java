fbbec29c020d131d024e8f71d571eb1118e7be67
Mete Unal <meteunal46@hotmail.com>
2026-07-31 17:41:07 -0400

    Initial commit

---
+++ src/test/java/com/example/investory_management/InvestoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investory_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
